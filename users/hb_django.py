766e5e73b0ff71445fe659420f9affd016dcccc1
himelbikon <skhimelhaque@gmail.com>
2021-11-15 02:04:19 +0600

    first commit

---
+++ users/hb_django.py
@@ -0,0 +1,40 @@
+import jwt
+import datetime
+from django.conf import settings
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import User
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            return None
+
+        try:
+            payload = jwt.decode(
+                token, settings.SECRET_KEY, algorithms=['HS256'])
+        except:
+            raise AuthenticationFailed('unauthenticated!')
+
+        user = User.objects.get(pk=payload['user_id'])
+        return (user, None)
+
+
+def generate_jwt(id):
+	payload = {
+            'user_id': id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            'iat': datetime.datetime.utcnow()
+        }
+
+	return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+
+def printer(x):
+	x = str(x)
+	sep = '----------------'
+	print(sep)
+	print(x)
+	print(sep)
---
+++ users/views.py
@@ -0,0 +1,50 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
+from .hb_django import JWTAuthentication, generate_jwt, printer
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect Password!')
+
+        token = generate_jwt(user.id)
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'message': 'success'
+        }
+
+        return response
+
+
+class Logout(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie(key='jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
+
+
+class Users(APIView):
+	authentication_classes = [JWTAuthentication]
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request):
+		users = User.objects.all()
+		serializer = UserSerializer(users, many=True)
+		return Response(serializer.data)
